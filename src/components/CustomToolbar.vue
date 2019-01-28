44b6e10ef5c1357d083ed20d3d41d37bfd77d700
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2019-01-29 00:20:23 +0330

    custom toolbar added

---
+++ src/components/CustomToolbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">Trustnet</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <a href="#">
+        <v-avatar color="indigo">
+          <v-icon dark>account_circle</v-icon>
+          <!--   <span class="white--text headline">FJ</span> -->
+        </v-avatar>
+      </a>
+
+    </v-toolbar>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      //
+    }
+  },
+  created() {
+    //build the href for avatar
+    //replace the text in span inside the avatar
+  }
+}
+</script>
