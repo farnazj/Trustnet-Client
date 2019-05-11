db2831fd851436936327fa81965dab35943ac6fc
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2019-05-11 22:41:19 +0430

    bug fix

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,5 +1,8 @@
-const baseURL = `http://localhost:3000`
+//const baseURL = `http://localhost:3000`;
+const baseURL = `http://developer.trustnet.csail.mit.edu`
+const VALIDITY_CODES = {'CONFIRMED': 1, 'REFUTED': -1, 'QUESTIONED': 0};
 
 export default {
-  baseURL
+  baseURL,
+  VALIDITY_CODES
 }
