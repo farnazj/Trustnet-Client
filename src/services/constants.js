101cd29ee7b867b2b666db59454eaa0cdd983b83
farnazj@mit.edu <farnaz.jahanbakhsh92@gmail.com>
2019-11-01 16:41:01 -0400

    separated fb app id

--- src/services/constants.js
+++ src/services/constants.js
@@ -2,9 +2,11 @@
 const baseURL = `http://developer.trustnet.csail.mit.edu`
 const VALIDITY_CODES = {'CONFIRMED': 1, 'REFUTED': -1, 'QUESTIONED': 0};
 const FBCommentsBaseURL = `http://trustnet.csail.mit.edu/test`;
+const FB_APP_ID = `1910706082368253`;
 
 export default {
   baseURL,
   VALIDITY_CODES,
-  FBCommentsBaseURL
+  FBCommentsBaseURL,
+  FB_APP_ID
 }
